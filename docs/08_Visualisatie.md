5c618e12002fa4587067307ac66b65d0cdb46e1e
Paul Janssen <p.janssen@geonovum.nl>
2025-03-25 16:23:49 +0100

    klaarzetten voor publicatie

--- docs/08_Visualisatie.md
+++ docs/08_Visualisatie.md
@@ -4,14 +4,14 @@ Visualisatie
 De specificaties voor het visualiseren van IMKL gegevens zijn opgenomen in
 aparte documenten:
 
-**Handreiking Visualisatieregels (PMKL).**
+[**Handreiking Visualisatieregels (PMKL).**](https://docs.geostandaarden.nl/kl/def-st-pmkl30-20250319/)
 
 Dit document bevat de regels ten behoeve van het opbouwen van een kaartbeeld
 van IMKL gegevens met betrekking tot de grafische variabelen kleur, vorm,
 grootte en transparantie. Daarnaast worden er schaalniveaus onderscheiden
 gerelateerd aan de visualisatie van IMKL gegevens.
 
-**IMKL Ordening attributen viewer.**
+**IMKL Ordening attributen viewer.** Gepubliceerd in [technisch register/visualisatie/imkl](https://register.geostandaarden.nl/?url=kabelsleidingen/imkl)
 
 Een excel document met regels voor de ordening van objectattributen die in
 een viewerapplicatie opgevraagd kunnen worden. Van de IMKL-objecten is
--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@ let respecConfig = {
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Informatiemodel Kabels en Leidingen (IMKL) - versie 3.0",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  //specStatus: "wv",                   // Werkversie
   // specStatus: "cv",               // Consultatieversie
-  //specStatus: "vv",               // Versie ter vaststelling
+  specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -36,13 +36,13 @@ let respecConfig = {
 
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  // publishDate: "2024-12-09",
+  publishDate: "2025-03-25",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2021-07-15",
-  previousMaturity: "def",
+  previousPublishDate: "2024-12-09",
+  previousMaturity: "cv",
   //-- TODO: de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
